00046f857eba495a0204c643fd2e872d69892252
ervie <obedencio.ervie17@gmail.com>
2026-05-25 02:40:27 +0800

    Add Whitenoise storage for static files in settings

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -110,12 +110,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
