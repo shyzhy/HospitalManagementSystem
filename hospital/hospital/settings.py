d2aa98f0ff090892677d2367f98a2a24dcb94ac6
shyzhy <shyzhyshyy@gmail.com>
2026-05-25 10:09:54 +0800

    Fix activation email settings

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -145,10 +145,13 @@ STORAGES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 # --- CORS ---
 CORS_ALLOW_ALL_ORIGINS = True
 
