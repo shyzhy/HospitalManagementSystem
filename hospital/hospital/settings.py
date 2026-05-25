2bbea142fc30277c9d82e73e0d79e287e36198cc
shyzhy <shyzhyshyy@gmail.com>
2026-05-25 10:55:58 +0800

    Fix activation email sending config

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -144,14 +144,19 @@ STORAGES = {
 
 # --- EMAIL (Gmail SMTP) ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.resend.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = os.environ.get(
+    'DEFAULT_FROM_EMAIL',
+    EMAIL_HOST_USER
+)
+
+EMAIL_TIMEOUT = 10
 # --- CORS ---
 CORS_ALLOW_ALL_ORIGINS = True
 
--- hospital/medflow/emails.py
+++ hospital/medflow/emails.py
@@ -1,7 +1,6 @@
 from djoser import email
-from django.template.loader import render_to_string
-from django.utils.html import strip_tags
 from django.conf import settings
+
 
 class CustomActivationEmail(email.ActivationEmail):
     template_name = "emails/activation.html"
@@ -14,20 +13,3 @@ class CustomActivationEmail(email.ActivationEmail):
             "http://localhost:3000"
         )
         return context
-
-    def send(self, to, *args, **kwargs):
-        context = self.get_context_data()
-
-        html_content = render_to_string(self.template_name, context)
-        plain_text = strip_tags(html_content)
-
-        self.subject = "Activate your MedFlow Account"
-        self.body = plain_text
-        self.from_email = kwargs.pop(
-            "from_email",
-            getattr(settings, "DEFAULT_FROM_EMAIL", None)
-        )
-        self.to = to
-
-        self.attach_alternative(html_content, "text/html")
-        super(email.ActivationEmail, self).send(to, *args, **kwargs)
